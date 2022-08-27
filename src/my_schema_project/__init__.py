c04c9624b641b6d89bed4cb71e9613325f992c1a
tonyfast <tony.fast@gmail.com>
2022-08-26 23:41:14 -0700

    include an implementation

--- src/my_schema_project/__init__.py
+++ src/my_schema_project/__init__.py
@@ -1,1 +1,8 @@
 from .models import my_schema_model
+
+class Item(my_schema_model.Model):
+    def __str__(self):
+        return F"## {self.name}\n\n> {self.description}\n\n"
+
+def get_item(name, description=None):
+    return Item(name=name, description=description)
